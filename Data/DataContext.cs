8f7cd713e67a11d11b30a6fd28d0f69dc35e7ef5
WTOPM <wtopm@list.ru>
2020-02-09 10:18:11 +0200

    dotnet ef migrations add AddedUserEntity dotnet ef databse update

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -7,5 +7,6 @@ namespace DatingApp.API.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base (options){}
         public DbSet<Weather> Weathers { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
