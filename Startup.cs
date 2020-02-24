ad0b9f79f0e36035b75fa92ce6a888fe10c08af2
WTOPM <wtopm@list.ru>
2020-02-24 13:38:06 +0200

    Cloudinary error

--- Startup.cs
+++ Startup.cs
@@ -34,6 +34,7 @@ namespace DatingApp.API
             services.AddControllers();
           services.AddMvc().AddNewtonsoftJson(options=> options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
             services.AddCors();
+            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
             services.AddAutoMapper(typeof(DatingRepository).Assembly);
             services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddScoped<IDatingRepository, DatingRepository>();
