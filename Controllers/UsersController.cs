6141c2858d06c8a99ad11b172c5bcb9dbecdf850
WTOPM <wtopm@list.ru>
2020-02-29 00:37:46 +0200

    LasActive

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -8,9 +8,11 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using System;
+using DatingApp.API.Helpers;
 
 namespace DatingApp.API.Controllers
 {
+    [ServiceFilter(typeof(LogUserActivity))]
     [Authorize]
     [Route("api/[controller]")]
     [ApiController]
