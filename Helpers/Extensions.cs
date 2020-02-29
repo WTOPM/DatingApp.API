6fe505eaa5d832b077d965184772c0aa12471589
WTOPM <wtopm@list.ru>
2020-02-29 22:03:46 +0200

    Add extensions

--- Helpers/Extensions.cs
+++ Helpers/Extensions.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace DatingApp.API.Helpers
 {
@@ -17,7 +18,10 @@ namespace DatingApp.API.Helpers
             int currentPage, int itemsPerPage, int totalItems, int totalPages)
             {
                 var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalItems, totalPages);
-                response.Headers.Add("Pagination", JsonConvert.SerializeObject(paginationHeader));
+                var camelCaseFormatter = new JsonSerializerSettings();
+                camelCaseFormatter.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                response.Headers.Add("Pagination",
+                    JsonConvert.SerializeObject(paginationHeader, camelCaseFormatter));
                 response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
             }
 
