1162a68c4b31505c00caff432df0e8e8e7ba59e9
WTOPM <wtopm@list.ru>
2020-03-03 15:54:52 +0200

    Implement MessageToReturnDto on AutoMapper

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
@@ -41,6 +42,25 @@ namespace DatingApp.API.Controllers
 
                 return Ok(messageFromRepo);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMessagesForUser(int userId,
+            [FromQuery]MessageParams messageParams)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            messageParams.UserId = userId;
+
+            var messagesFromRepo = await _repo.GetMessagesForUser(messageParams);
+
+            var messages = _mapper.Map<IEnumerable<MessageToReturnDto>>(messagesFromRepo);
+
+            Response.AddPagination(messagesFromRepo.CurrentPage, messagesFromRepo.PageSize,
+                messagesFromRepo.TotalCount, messagesFromRepo.TotalPages);
+            return Ok(messages);
+        }
+
         [HttpPost]
         public async Task<IActionResult> CreateMessage(int userId, MessageForCreationDto messageForCreationDto)
         {
