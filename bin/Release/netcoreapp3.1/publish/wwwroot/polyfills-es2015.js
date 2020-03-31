9bc661169ae3255a511b6ea4b2cd197e95b0298f
Maksym Makar <wtopm@list.ru>
2020-03-31 22:02:42 +0300

    Revert "Update"
    This reverts commit 17f579f183c0064e24bf51ed08e3c8605a25337e.

--- bin/Release/netcoreapp3.1/publish/wwwroot/polyfills-es2015.js
+++ bin/Release/netcoreapp3.1/publish/wwwroot/polyfills-es2015.js
@@ -3024,7 +3024,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\WTOPM\Desktop\GIT\DatingApp\DatingApp-SPA\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/annaodnostalko/Desktop/GIT/DatingApp/DatingApp-SPA/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
